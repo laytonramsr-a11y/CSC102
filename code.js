e5f592a586c3df066d28aa7cdf4c7135a9d3fffc
Layton Ramsey <laytonramsr@gmail.com>
2026-05-23 15:24:21 -0400

    2.3 assignment update
    added another button with warning alerts

--- code.js
+++ code.js
@@ -1,3 +1,6 @@
 function updateText(){
     document.getElementById("heading").innerHTML = "Kaboom!";
 }
+function updateText2(subheading){
+    document.getElementById("subheading").innerHTML = " Ready to play!" + subheading;
+}
