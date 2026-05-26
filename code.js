c8a13bf31d77bcd5b6fbcb94b9485729ea889f5f
Layton Ramsey <laytonramsr@gmail.com>
2026-05-26 18:53:23 -0400

    Fix my mistakes
    5/26/26

--- code.js
+++ code.js
@@ -1,24 +1,25 @@
-function updateText(){
-    //udate the text of the heading//
-    document.getElementById("heading").innerHTML = "Kaboom!";
-    //when the button is clicked, the heading will change to "Kaboom!"//
-}
-function updateText2(subheading){
-    //update the text of the subheading//
-    document.getElementById("subheading").innerHTML = " Ready to play!" + subheading;
-    //when the button is clicked, the subheading will change to "Ready to play!" followed by the text in the parentheses//
-}
-function calculateSumOutput(die1, die2) {
-            var sum = die1 + die2;
-            if (sum == 7 || sum == 11) {
-                console.log("You Win!");
-            } else if (die1 == die2 && die1 % 2 == 0) {
-                console.log("Doubles! You get to roll again.");
-            } else if (sum == 2 || sum == 3 || sum == 12) {
-                console.log("You Lose!");
-            } else {
-                console.log("No immediate result: sum = " + sum);
-            }
-            // return the sum for further use
-            return sum;
+   function playCraps()
+    {
+	    var num1 = Math.floor(Math.random() * 6) + 1;
+  
+	    var num2 = Math.floor(Math.random() * 6) + 1;
+
+	    var sum = num1 + num2;
+
+        document.getElementById("crapsResult").innerHTML = "Craps Result: " + sum;
+    
+	    if (sum == 7 || sum == 11)
+	    {
+		document.getElementById("crapsResult").innerHTML = "CRAPS - You Lose! The result is: " + sum;
         }
+	    else if (num1 == num2 && num1 % 2 == 0)
+	    {
+		document.getElementById("crapsResult").innerHTML = "You Won! The dice values are both: " + num1;
+        }
+	    else
+	    {
+		document.getElementById("crapsResult").innerHTML = "You Pushed! Die 1: " + num1 + " Die 2 " + num2;
+	    }
+
+    }
+        
