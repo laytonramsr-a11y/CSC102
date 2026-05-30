9492ebe24a48ff671047147053d65e59bb813484
Layton Ramsey <laytonramsr@gmail.com>
2026-05-30 14:29:45 -0400

    trying to follow the video
    lost

--- code.js
+++ code.js
@@ -1,65 +1,36 @@
-   function playCraps()
-    {
-	    var num1 = Math.floor(Math.random() * 6) + 1;
-  
-	    var num2 = Math.floor(Math.random() * 6) + 1;
+function playCraps() {
 
-	    var sum = num1 + num2;
+    var num1 = Math.floor(Math.random() * 6) + 1;
 
-        document.getElementById("crapsResult").innerHTML = "Craps Result: " + sum;
-    
-	    if (sum == 7 || sum == 11)
-	    {
-		document.getElementById("crapsResult").innerHTML = "CRAPS - You Lose! The result is: " + sum;
-        }
-	    else if (num1 == num2 && num1 % 2 == 0)
-	    {
-		document.getElementById("crapsResult").innerHTML = "You Won! The dice values are both: " + num1;
-        }
-	    else
-	    {
-		document.getElementById("crapsResult").innerHTML = "You Pushed! Die 1: " + num1 + " Die 2: " + num2;
-	    }
+    var num2 = Math.floor(Math.random() * 6) + 1;
 
+    var sum = num1 + num2;
+
+    var result = "Craps Result: " + sum;
+
+    if (sum == 7 || sum == 11) {
+        result = "CRAPS - You Lose! The result is: " + sum;
+    } else if (num1 == num2 && num1 % 2 == 0) {
+        result = "You Won! The dice values are both: " + num1;
+    } else {
+        result = "You Pushed! Die 1: " + num1 + " Die 2: " + num2;
     }
 
-	function validateForm()
-        {
-            var fname;
-                
-            var lname;
-                
-            var sum = fname + " " + lname;
+    document.getElementById("crapsResult").innerHTML = result;
+}
 
-            var areacode;
-                theareacode = 5;
+function validateInput(event) {
+    console.log("Validating input...");
+	return false;
 
-            if (sum > 20)
-            {    
-             document.getElementById("warning").innerHTML = "Invalid"+ sum.length;
-            }
-            else if (sum == 20)
-            {
-                document.getElementById("warning").innerHTML = "Valid" + sum.length;
-            }
+	window.addEventListener("DOMContentLoaded", (event) => {
+    	const form = document.getElementById("myForm");
+   		 if (form) {
+        	form.addEventListener("submit", validateInput);
+    }
+	});
 
-          areacode = document.getElementById("areacode").value;
-
-            if (areacode > 5)
-            {
-                document.getElementById("warning").innerHTML = "!Invalid" + areacode.length;
-            }
-            else if (areacode == 5)
-            {
-                document.getElementById("warning").innerHTML = "Valid" + areacode.length;
-            }
-        }
-
-
-
-	
- 
-
-
+    
+}
 
 	
