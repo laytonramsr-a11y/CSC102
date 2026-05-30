928e7cd19d2139b0fdb3983bab8b8a75dbf1f64c
Layton Ramsey <laytonramsr@gmail.com>
2026-05-30 05:04:54 -0400

    Working on 3.1 & 3.2 assignments
    Completed assignment 3.1, still troubleshooting 3.3

--- code.js
+++ code.js
@@ -18,8 +18,47 @@
         }
 	    else
 	    {
-		document.getElementById("crapsResult").innerHTML = "You Pushed! Die 1: " + num1 + " Die 2 " + num2;
+		document.getElementById("crapsResult").innerHTML = "You Pushed! Die 1: " + num1 + " Die 2: " + num2;
 	    }
 
     }
-        
+
+	function validateForm()
+        {
+            var fname = Math.floor(Math.length() = 9) + 1;
+
+            var lname = Math.floor(Math.length() = 9) + 1; 
+
+            var fullname = fname + " " + lname;
+
+            var areacode = document.getElementById("areacode").value;
+
+            if (fullname.length > 20)
+            {    
+             document.getElementById("warning").innerHTML = "Invalid";
+            }
+            else if (fullname.length == 20)
+            {
+                document.getElementById("warning").innerHTML = "Valid";
+            }
+
+          areacode = document.getElementById("areacode").value;
+
+            if (areacode.length > 5)
+            {
+                document.getElementById("warning").innerHTML = "!Invalid";
+            }
+            else if (areacode.length == 5)
+            {
+                document.getElementById("warning").innerHTML = "Valid";
+            }
+        }
+
+
+
+	
+ 
+
+
+
+	
